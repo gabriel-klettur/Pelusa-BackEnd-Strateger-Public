4976e24f88ed18582fc3eafeea0f9114a9e0f13c
Gabriel Roca <blast.blackstone@gmail.com>
2024-11-30 19:49:09 +0000

    Fix(siteground): updated import deprecated 'declarative_base'

--- app/siteground/base.py
+++ app/siteground/base.py
@@ -1,24 +1,9 @@
-# Path: app/siteground/base.py
+from sqlalchemy.orm import declarative_base
 
-from sqlalchemy.ext.declarative import declarative_base
-
-# Base for alarms database
 BaseAlarmas = declarative_base()
-
-# Base for strategies database
 BaseEstrategias = declarative_base()
-
-# Base for diary database
 BaseDiary = declarative_base()
-
-# Base for positions database
 BasePositions = declarative_base()
-
-# Base for accounts database
 BaseAccounts = declarative_base()
-
-# Base for k-line data in database
 BaseKLineData = declarative_base()
-
-# Base for orders database
 BaseOrders = declarative_base()
