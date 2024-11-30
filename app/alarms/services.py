388fa9d8c6d68337732d2b3c8749b01bbc86494c
Gabriel Roca <blast.blackstone@gmail.com>
2024-11-30 19:54:33 +0000

    Docs(alarms): added comments in 'services.py'

--- app/alarms/services.py
+++ app/alarms/services.py
@@ -1,4 +1,4 @@
-#Path: app/alarms/controllers.py
+#Path: app/alarms/services.py
 
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
@@ -8,6 +8,21 @@ from app.alarms.schemas import AlarmResponse                            #Schemas
 
 
 async def fetch_alarms(limit: int, offset: int, latest: bool, db: Session):
+    """
+    Fetch alarms from the database with given parameters.
+
+    Args:
+        limit (int): The maximum number of alarms to fetch.
+        offset (int): The number of alarms to skip before starting to fetch.
+        latest (bool): Whether to fetch the latest alarms.
+        db (Session): The database session to use for fetching alarms.
+
+    Returns:
+        List[AlarmResponse]: A list of validated alarm responses.
+
+    Raises:
+        HTTPException: If there is an error fetching the alarms.
+    """
     try:
         alarms = await get_alarms(db, limit=limit, offset=offset, latest=latest)
         return [AlarmResponse.model_validate(alarm) for alarm in alarms]
