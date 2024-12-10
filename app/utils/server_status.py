0fabd356218372280e2e3387a523417487affb9d
Gabriel Roca <blast.blackstone@gmail.com>
2024-12-10 17:39:39 +0000

    feat(utils/server_status): Changed 'logger.info' output by print for regular server working.

--- app/utils/server_status.py
+++ app/utils/server_status.py
@@ -2,11 +2,13 @@
 
 import asyncio
 from loguru import logger
+from datetime import datetime
 
 async def log_server_status():
     while True:
         try:            
-            logger.info("Server is running smoothly")
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"[{current_time}] Server is running smoothly")
         except Exception as e:
             logger.error(f"Server status check failed: {e}")
         await asyncio.sleep(1) 
